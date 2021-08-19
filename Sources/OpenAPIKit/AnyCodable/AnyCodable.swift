4f406328cb4543fe9d27325949f01914ca2c9863
Mendez <mndzup@88665a076941.ant.amazon.com>
2021-08-19 14:19:34 -0400

    Change ordering for decoding any

--- Sources/OpenAPIKit/AnyCodable/AnyCodable.swift
+++ Sources/OpenAPIKit/AnyCodable/AnyCodable.swift
@@ -133,16 +133,16 @@ extension AnyCodable: Decodable {
 
         if container.decodeNil() {
             self.init(NSNull())
-        } else if let bool = try? container.decode(Bool.self) {
-            self.init(bool)
+        } else if let string = try? container.decode(String.self) {
+            self.init(string)
         } else if let int = try? container.decode(Int.self) {
             self.init(int)
         } else if let uint = try? container.decode(UInt.self) {
             self.init(uint)
         } else if let double = try? container.decode(Double.self) {
             self.init(double)
-        } else if let string = try? container.decode(String.self) {
-            self.init(string)
+        } else if let bool = try? container.decode(Bool.self) {
+            self.init(bool)
         } else if let array = try? container.decode([AnyCodable].self) {
             self.init(array.map { $0.value })
         } else if let dictionary = try? container.decode([String: AnyCodable].self) {
